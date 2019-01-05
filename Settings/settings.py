e3d63571928ec0041a5b1352b0e439526d3feb22
Gilvan Almeida <g6almeida@hotmail.com>
2019-01-05 00:33:27 -0200

    Added settings class

---
+++ Settings/settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    """Uma Classe para armazenar todas as configurações da Invasão Alienígina"""
+    def __init__(self):
+        """Inicializa as configuração do jogo."""
+        # Configurações da tela
+        self.screen_wigth  = 1200
+        self.screen_height = 800
+        self.bg_color      = (230, 230, 230)
--- alien_invasion.py
+++ alien_invasion.py
@@ -1,22 +1,33 @@
 import sys
 import pygame
 
+from Settings.settings import Settings
+
+
 def run_game():
     # Inicializa o jogo e cria um objeto para a tela
     pygame.init()
+    ai_settings = Settings()
     # dimensões da tela
-    screen = pygame.display.set_mode((1200,800))
-    # Titlulo da Janela
+    screen = pygame.display.set_mode((ai_settings.screen_wigth, ai_settings.screen_height))
+    # Títlulo da Janela
     pygame.display.set_caption("Alien Invasion")
+
+    # Define a cor de fundo no formato rgb
+    pg_color = (ai_settings.bg_color)
 
     # Inicia o loop principal do jogo
     while True:
-        #Observa eventos do teclado e mouse
+        # Observa eventos do teclado e mouse
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+        # Redesenha a tela a cada passagem pelo laço
+        screen.fill(pg_color)
+
         # Deixa a tela mais recente visível
         pygame.display.flip()
+
 
 run_game()
