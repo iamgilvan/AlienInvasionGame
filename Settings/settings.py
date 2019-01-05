ffdf0027ee5ea98c9ef81e810eb729547c99a9a5
Gilvan Almeida <g6almeida@hotmail.com>
2019-01-05 01:19:11 -0200

    Translate comments

--- Settings/settings.py
+++ Settings/settings.py
@@ -1,8 +1,8 @@
 class Settings():
-    """Uma Classe para armazenar todas as configurações da Invasão Alienígina"""
+    """A class to store all the settings of Alien Invasion"""
     def __init__(self):
-        """Inicializa as configuração do jogo."""
-        # Configurações da tela
+        """Initializing the settings"""
+        # Screen settings
         self.screen_wigth  = 1200
         self.screen_height = 800
         self.bg_color      = (230, 230, 230)
